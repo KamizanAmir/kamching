0db409687c934f62f559eee57b1dbfe7d024d0a2
KamizanAmir <kamizan980505@gmail.com>
2023-09-12 18:49:56 +0800

    add onboarding img

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, Image, StyleSheet } from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
 
 const OnboardingScreen = ({ navigation }) => {
@@ -10,19 +10,19 @@ const OnboardingScreen = ({ navigation }) => {
             pages={[
             {
                 backgroundColor: '#fff',
-                image: <Image source={require('./images/circle.png')} />,
+                image: <Image source={require('.../assets/onboarding-img1.png')} />,
                 title: 'Onboarding',
                 subtitle: 'Done with React Native Onboarding Swiper',
             },
             {
                 backgroundColor: '#fff',
-                image: <Image source={require('./images/circle.png')} />,
+                image: <Image source={require('.../assets/onboarding-img1.png')} />,
                 title: 'Onboarding2',
                 subtitle: 'Done with React Native Onboarding Swiper',
             },
             {
                 backgroundColor: '#fff',
-                image: <Image source={require('./images/circle.png')} />,
+                image: <Image source={require('.../assets/onboarding-img1.png')} />,
                 title: 'Onboarding3',
                 subtitle: 'Done with React Native Onboarding Swiper',
             },
